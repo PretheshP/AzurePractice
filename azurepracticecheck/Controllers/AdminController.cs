217bb5b3675106d7fdf08bfb8c1d5f03601a8e7e
PretheshP <80540324+PretheshP@users.noreply.github.com>
2022-09-12 23:57:36 +0530

    Update AdminController.cs

--- azurepracticecheck/Controllers/AdminController.cs
+++ azurepracticecheck/Controllers/AdminController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,17 +7,18 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using azurepracticecheck.Models;
 using azurepracticecheck;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+//using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace azurepracticecheck.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize(AuthenticationSchemes =JwtBearerDefaults.AuthenticationScheme, Roles = "Admin")]
+    //[Authorize(AuthenticationSchemes =JwtBearerDefaults.AuthenticationScheme, Roles = "Admin")]
     //[Authorize(Roles ="Admin")]
     public class AdminController : ControllerBase
     {
-   
+        //private const object JwtBearerDefaults;
+
         // GET: api/Admin
         [HttpGet]
         public IEnumerable<MenuItem> Get()
@@ -36,3 +37,44 @@ namespace azurepracticecheck.Controllers
 
     }
 }
+
+
+
+// ﻿using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using Microsoft.AspNetCore.Authorization;
+// using Microsoft.AspNetCore.Http;
+// using Microsoft.AspNetCore.Mvc;
+// using azurepracticecheck.Models;
+// using azurepracticecheck;
+// using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+// namespace azurepracticecheck.Controllers
+// {
+//     [Route("api/[controller]")]
+//     [ApiController]
+//     [Authorize(AuthenticationSchemes =JwtBearerDefaults.AuthenticationScheme, Roles = "Admin")]
+//     //[Authorize(Roles ="Admin")]
+//     public class AdminController : ControllerBase
+//     {
+   
+//         // GET: api/Admin
+//         [HttpGet]
+//         public IEnumerable<MenuItem> Get()
+//         {
+//             return MenuItemOperations.GetConnection();
+//         }
+
+        
+//         [HttpPut("{id}")]
+//         public IActionResult Put(int id, [FromBody] MenuItem menu)
+//         {
+//             MenuItemOperations.Update(id, menu);
+//             return Ok();
+//         }
+
+
+//     }
+// }
