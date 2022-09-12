71c1226dbefdeb952e5b318b4b699bcdd1f8423a
PretheshP <80540324+PretheshP@users.noreply.github.com>
2022-09-12 23:59:10 +0530

    Update AuthController.cs

--- azurepracticecheck/Controllers/AuthController.cs
+++ azurepracticecheck/Controllers/AuthController.cs
@@ -1,6 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
+//using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Reflection.Metadata;
 using System.Security.Claims;
@@ -20,23 +20,71 @@ namespace azurepracticecheck.Controllers
         private IConfiguration _config;
 
         public AuthController(IConfiguration config) { _config = config; }
-        [HttpGet("{id}")] public IActionResult Get(int id) { return Ok(GenerateJSONWebToken(id)); }
+        //[HttpGet("{id}")] public IActionResult Get(int id) { return Ok(GenerateJSONWebToken(id)); }
 
-        private string GenerateJSONWebToken(int userId)
-        {
-            string userRole = "";
+        //private string GenerateJSONWebToken(int userId)
+        //{
+        //    string userRole = "";
 
-            if (userId == 1) userRole = "Admin";
+        //    if (userId == 1) userRole = "Admin";
 
-            else if (userId != -1 && userId != 1) userRole = "Customer";
+        //    else if (userId != -1 && userId != 1) userRole = "Customer";
 
-            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"])); var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+        //    var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"])); var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
 
-            var claims = new List<Claim> { new Claim(ClaimTypes.Role, userRole), new Claim("UserId", userId.ToString()) };
+        //    var claims = new List<Claim> { new Claim(ClaimTypes.Role, userRole), new Claim("UserId", userId.ToString()) };
 
-            var token = new JwtSecurityToken(_config["Jwt:Issuer"], _config["Jwt:Issuer"], claims: claims, expires: DateTime.Now.AddMinutes(10), signingCredentials: credentials); return new JwtSecurityTokenHandler().WriteToken(token);
+        //    //var token = new JwtSecurityToken(_config["Jwt:Issuer"], _config["Jwt:Issuer"], claims: claims, expires: DateTime.Now.AddMinutes(10), signingCredentials: credentials); return new JwtSecurityTokenHandler().WriteToken(token);
 
-        }
+        //}
     }
 }
+
+
+
+
+
+
+
+// ﻿using System;
+// using System.Collections.Generic;
+// using System.IdentityModel.Tokens.Jwt;
+// using System.Linq;
+// using System.Reflection.Metadata;
+// using System.Security.Claims;
+// using System.Text;
+// using System.Threading.Tasks;
+// using Microsoft.AspNetCore.Http;
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.Extensions.Configuration;
+// using Microsoft.IdentityModel.Tokens;
+
+// namespace azurepracticecheck.Controllers
+// {
+//     [Route("api/[controller]")]
+//     [ApiController]
+//     public class AuthController : ControllerBase
+//     {
+//         private IConfiguration _config;
+
+//         public AuthController(IConfiguration config) { _config = config; }
+//         [HttpGet("{id}")] public IActionResult Get(int id) { return Ok(GenerateJSONWebToken(id)); }
+
+//         private string GenerateJSONWebToken(int userId)
+//         {
+//             string userRole = "";
+
+//             if (userId == 1) userRole = "Admin";
+
+//             else if (userId != -1 && userId != 1) userRole = "Customer";
+
+//             var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"])); var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+//             var claims = new List<Claim> { new Claim(ClaimTypes.Role, userRole), new Claim("UserId", userId.ToString()) };
+
+//             var token = new JwtSecurityToken(_config["Jwt:Issuer"], _config["Jwt:Issuer"], claims: claims, expires: DateTime.Now.AddMinutes(10), signingCredentials: credentials); return new JwtSecurityTokenHandler().WriteToken(token);
+
+//         }
+//     }
+// }
 
